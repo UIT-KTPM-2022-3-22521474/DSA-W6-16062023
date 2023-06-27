30a0bc9632b07457c2e5069a1d4113cbc016ad34
Terry <se.terry.2004@gmail.com>
2023-06-27 13:48:08 +0700

    Update by seterry

--- 03. BFS/BFS.cpp
+++ 03. BFS/BFS.cpp
@@ -1,100 +1,106 @@
 #include <iostream>
-#include <queue>
-#include <unordered_map>
 #include <vector>
-#include <string>
-#include <numeric>
 #include <algorithm>
+#include <stack>
+#include <map>
 using namespace std;
 
-vector<string> bfs(unordered_map<string, vector<string>>& graph, const string& start, const string& end) 
-{
-    vector<string> path;
-    queue<vector<string>> q;
-    q.push({ start });
-    if (start == end)
-    {
-        return { start };
-    }    
-    while (!q.empty())
-    {
-        path = q.front();
-        q.pop();
-        string node = path.back();
-        if (node == end)
-        {
-            return path;
-        }
-        if (graph.find(node) != graph.end())
-        {
-            vector<string> neighbors = graph[node];
-            for (const string& neighbor : neighbors) 
-            {
-                if (find(path.begin(), path.end(), neighbor) == path.end()) 
-                {
-                    vector<string> new_path = path;
-                    new_path.push_back(neighbor);
-                    q.push(new_path);
-                }
-            }
-        }
-    }
-    return {};
+bool cmp(pair<char, int> p1, pair<char, int> p2) {
+    return p1.second < p2.second;
 }
 
-vector<string> find_paths(unordered_map<string, vector<string>>& graph, vector<pair<string, string>>& pairs)
-{
-    vector<string> paths;
-    for (const auto& pair : pairs)
-    {
-        const string& start = pair.first;
-        const string& end = pair.second;
-        vector<string> path = bfs(graph, start, end);
-        if (!path.empty())
-        {
-            paths.push_back(accumulate(path.begin(), path.end(), string(),
-                [](const string& acc, const string& node) {
-                    return acc.empty() ? node : acc + " " + node;
-                }));
-        }
-        else 
-        {
-            paths.push_back("no_path");
+void Input(int v, map<char, int>& index, int e, vector< vector<int> >& G) {
+    char str;
+    for (int i = 0; i < v; i++) {
+        cin >> str;
+        index[str] = i;
+    }
+    char s1, s2;
+    int a, b;
+    map<char, int>::iterator it;
+    while (e--) {
+        cin >> s1 >> s2;
+        it = index.find(s1);
+        a = it->second;
+        it = index.find(s2);
+        b = it->second;
+        G[a][b] = 1;
+    }
+}
+
+int Print(vector< vector<int> > G, int v, int X, int Y) {
+    if (G[Y][X] != 0) {
+        return Y;
+    }
+    for (int index = v - 1; index >= 0; index--) {
+        if (G[index][X] != 0) {
+            G[index][X] = 0;
+            return index;
         }
     }
-    return paths;
+    return -1;
+}
+
+bool Find(vector< vector<int> >& G, int v, int X) {
+    for (int index = 0; index < v; index++) {
+        if (G[X][index] != 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void BFS(vector< vector<int> > G, map<char, int> index, int v) {
+    char s1, s2;
+    cin >> s1 >> s2;
+
+    vector< pair<char, int> > vec(index.begin(), index.end());
+    sort(vec.begin(), vec.end(), cmp);
+
+    map<char, int>::iterator it;
+    it = index.find(s1);
+    int X = it->second;
+    it = index.find(s2);
+    int Y = it->second;
+
+    if (Find(G, v, X) == false) {
+        cout << "no_path" << endl;
+        return;
+    }
+
+    int addr = 0;
+    stack<int> myStack;
+    myStack.push(Y);
+
+    while (true) {
+        addr = Print(G, v, Y, X);
+        myStack.push(addr);
+        if (addr == X) {
+            break;
+        }
+        Y = addr;
+    }
+
+    while (!myStack.empty()) {
+        addr = myStack.top();
+        cout << vec[addr].first << ' ';
+        myStack.pop();
+    }
+
+    cout << "\n";
 }
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    unordered_map<string, vector<string>> graph;
-    for (int i = 0; i < n; ++i)
-    {
-        string vertex;
-        cin >> vertex;
-        graph[vertex] = vector<string>();
-    }
-    for (int i = 0; i < m; ++i)
-    {
-        string u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-    }
-    int k;
-    cin >> k;
-    vector<pair<string, string>> pairs;
-    for (int i = 0; i < k; ++i) 
-    {
-        string start, end;
-        cin >> start >> end;
-        pairs.push_back(make_pair(start, end));
-    }
-    vector<string> paths = find_paths(graph, pairs);
-    for (const string& path : paths)
-    {
-        cout << path << endl;
+    int v, e;
+    cin >> v >> e;
+    vector< vector<int> > G(v, vector<int>(v, 0));
+    map<char, int> v_index;
+    Input(v, v_index, e, G);
+    int N;
+    cin >> N;
+    while (N--) {
+        BFS(G, v_index, v);
     }
     return 0;
 }
